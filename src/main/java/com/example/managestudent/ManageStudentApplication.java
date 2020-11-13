8755688c0eea2e32914db6cf2c875c52188ab9c6
Mulapese <phuoc875@gmail.com>
2020-11-13 08:03:14 +0700

    Update swagger

--- src/main/java/com/example/managestudent/ManageStudentApplication.java
+++ src/main/java/com/example/managestudent/ManageStudentApplication.java
@@ -3,10 +3,15 @@ package com.example.managestudent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -17,8 +22,21 @@ public class ManageStudentApplication {
     }
 
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_12).select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.managestudent")).build();
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.managestudent"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "FUni Management",
+                "Manage student and course.",
+                "Beta 0.0.1",
+                "Terms of service",
+                new Contact("Nguyen Ngoc Phuoc", "www.phuoc308.com", "phuoc875@gmail0.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 }
